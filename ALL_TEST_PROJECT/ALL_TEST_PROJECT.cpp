8b2b16ed741b884660b66e601b5458d01de774d4
orizonti <orizonti@inbox.ru>
2022-07-20 21:17:25 +0300

    thread chrono test output

---
+++ ALL_TEST_PROJECT/ALL_TEST_PROJECT.cpp
@@ -0,0 +1,12 @@
+﻿// ALL_TEST_PROJECT.cpp: определяет точку входа для приложения.
+//
+
+#include "ALL_TEST_PROJECT.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ STD_CHRONO_TEST/STD_CHRONO_TEST.cpp
@@ -0,0 +1,45 @@
+﻿// ALL_TEST_PROJECT.cpp: определяет точку входа для приложения.
+//
+
+#include "STD_CHRONO_TEST.h"
+#include <chrono>
+
+using namespace std;
+
+void TestTime(int Count)
+{
+
+	std::chrono::time_point<std::chrono::high_resolution_clock> TimePoint = std::chrono::high_resolution_clock::now();
+
+
+		for (int x = 0; x<Count; x = x + 4)
+		{
+			int t = 0;
+			int y = 2;
+			int z = t + y;
+
+		}
+
+		std::chrono::time_point<std::chrono::high_resolution_clock> TimePoint2 = std::chrono::high_resolution_clock::now();
+
+		std::chrono::duration<double> Dur = TimePoint2 - TimePoint;
+		std::cout << "Duration - " << Dur.count() <<"  count -    "<< Count << "\n";
+
+};
+
+
+int main(int argc, char* argv[])
+{
+	cout << "STD CHRONO TEST" << endl;
+	TestTime(1000000);
+	TestTime(2000000);
+	TestTime(3000000);
+	TestTime(4000000);
+	TestTime(5000000);
+	TestTime(6000000);
+	TestTime(7000000);
+
+
+	return 0;
+}
+
