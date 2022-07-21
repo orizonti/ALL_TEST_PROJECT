9a4312e39cc3e5af58edd9084577394f5c51f01e
orizonti <orizonti@inbox.ru>
2022-07-21 21:32:41 +0300

    Thread example added

--- STD_THREAD_TEST/STD_THREAD_TEST.cpp
+++ STD_THREAD_TEST/STD_THREAD_TEST.cpp
@@ -3,32 +3,91 @@
 
 #include "STD_THREAD_TEST.h"
 #include <thread>
+#include <chrono>
 
 using namespace std;
 
-void foo()
+void TestPow(int input, int &output) // return reslut by reference
 {
-    // simulate expensive operation
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    cout << "THREAD ID - " << this_thread::get_id() << " THREAD WORK" << endl;
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    output = input * input;
+    cout << "THREAD ID - " << this_thread::get_id() << " THREAD END" << endl;
 }
- 
-void bar()
+
+int TestSum(int x, int y)
 {
-    // simulate expensive operation
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    return x + y;
+}
+
+class MyClass
+{
+    public:
+    void DoWork();
+    void DoWork2(int a);
+};
+
+void MainWork()
+{
+
+    for( size_t i = 0; i < 10; i++)
+    {
+        cout << "THREAD ID - " << this_thread::get_id() << " MAIN WORK" << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    } 
 }
  
 int main()
 {
-    std::cout << "starting first helper...\n";
-    std::thread helper1(foo);
+    //===================================================
+    cout << "TEST THREAD RETURN REF";
+    int output = 0; thread t1(TestPow,2,ref(output));
+    cout << "RESULT - " << output << endl;
+    cout << "===================" << endl;
+
+    MainWork();
+    
+    t1.join();
+
+    cout << "===================================================" << endl;
+    cout << "TEST THREAD RETURN FROM LAMBDA" << endl;
+
+    int result = 0;
+    thread t2( [&result](){ result = TestSum(3,4); } );
+    t2.join();
+
+    cout << "RESULT - " << result << endl;
+    cout << "===================================================" << endl;
+
+    cout << "TEST THREAD CLASS METHOD" << endl;
+
+    MyClass TestObject;
+    thread t3( [&TestObject](){ TestObject.DoWork(); } );
+    thread t4( &MyClass::DoWork2, TestObject,2 );
+
+    MainWork();
+    t3.join(); 
+    t4.join();
+    cout << "===================================================" << endl;
+
+}
+
+
+void MyClass::DoWork()
+{
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    cout << "ID thread :" << this_thread::get_id() << "MY CLASS DO WORK" << endl;
+    this_thread::sleep_for(chrono::milliseconds(5000));
+    cout << "ID thread :" << this_thread::get_id() << "MY CLASS STOPPED WORK" << endl;
  
-    std::cout << "starting second helper...\n";
-    std::thread helper2(bar);
+}
+
+void MyClass::DoWork2(int a)
+{
+    cout << "DO WORK 2 INPUT PARAMETER - " << a << endl;
+    this_thread::sleep_for(chrono::milliseconds(2000));
+    cout << "ID thread :" << this_thread::get_id() << "MY CLASS DO WORK" << endl;
+    this_thread::sleep_for(chrono::milliseconds(5000));
+    cout << "ID thread :" << this_thread::get_id() << "MY CLASS STOPPED WORK" << endl;
  
-    std::cout << "waiting for helpers to finish..." << std::endl;
-    helper1.join();
-    helper2.join();
- 
-    std::cout << "done!\n";
 }
