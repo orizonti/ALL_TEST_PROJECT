db2399f5ac8c64f53796a8f6a7692e2f37ed1060
Alexander <orizonti@yandex.ru>
2022-11-14 20:13:09 +0700

    RANGES_TEST ADD

--- MOVE_SEMANTICS_TEST/MOVE_SEMANTICS.cpp
+++ MOVE_SEMANTICS_TEST/MOVE_SEMANTICS.cpp
@@ -2,6 +2,9 @@
 //
 #include "MOVE_SEMANTICS.h"
 #include <vector>
+// forward example
+#include <utility>      // std::forward
+#include <iostream>     // std::cout
 
 using namespace std;
 
@@ -22,24 +25,47 @@ class MyClassTest
 
 
 
+// function with lvalue and rvalue reference overloads:
+void overloaded (const int& x) {std::cout << "[lvalue]";}
+void overloaded (int&& x) {std::cout << "[rvalue]";}
+
+// function template taking rvalue reference to deduced type:
+template <class T> void fn (T&& x) {
+  overloaded (x);                   // always an lvalue
+  overloaded (std::move(x));                   // always an lvalue
+  overloaded (std::forward<T>(x));  // rvalue if argument is rvalue
+  std::cout << endl;
+}
+
  
 int main()
 {
-  cout << "============================" << endl;
-  MyClassTest t = 20;
-  cout << "-----------------" << endl;
-  MyClassTest t2 = MyClassTest(30);
-  cout << "-----------------" << endl;
-  MyClassTest t3 = t2;
-  cout << "============================" << endl;
-  cout << "COPY TEST" << endl;
-  t = t2;
-  cout << "============================" << endl;
+
+  int a;
+
+  std::cout << endl;
+  std::cout << "calling fn with lvalue: "; fn (a);
+  std::cout << "calling fn with rvalue: "; fn (0);
+  std::cout << "calling fn with lvalue: "; fn (std::move(a));
+
+
+  //cout << "============================" << endl;
+  //MyClassTest t = 20;
+  //cout << "-----------------" << endl;
+  //MyClassTest t2 = MyClassTest(30);
+  //cout << "-----------------" << endl;
+  //MyClassTest t3 = t2;
+  //cout << "============================" << endl;
+  //cout << "COPY TEST" << endl;
+  //t = t2;
+  //cout << "============================" << endl;
   
-  cout << "VECTOR TEST" << endl;
-  std::vector<MyClassTest> vec; vec.reserve(2);
-  vec.push_back(MyClassTest()); vec.push_back(MyClassTest());
-  cout << "============================" << endl;
-  std::vector<MyClassTest> vec2{2,2,2,2,2};
+  //cout << "VECTOR TEST" << endl;
+  //std::vector<MyClassTest> vec; vec.reserve(2);
+  //vec.push_back(MyClassTest()); vec.push_back(MyClassTest());
+  //cout << "============================" << endl;
+  //std::vector<MyClassTest> vec2{2,2,2,2,2};
+
+  return 0;
 }
 
---
+++ RANGES_TEST/RANGES_TEST.cpp
@@ -0,0 +1,16 @@
+﻿// ALL_TEST_PROJECT.cpp: определяет точку входа для приложения.
+//
+
+#include "RANGES_TEST.h"
+#include <vector>
+#include <functional>
+#include <numeric>
+
+using namespace std;
+namespace ph = std::placeholders;
+
+
+int main(int argc, char* argv[])
+{
+}
+
